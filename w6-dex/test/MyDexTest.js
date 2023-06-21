522bc8dc40fce985fadc28b2570e5dd4972c9582
cqyang <cqyang@glp.com>
2023-06-21 17:24:40 +0800

    test dex

--- w6-dex/test/MyDexTest.js
+++ w6-dex/test/MyDexTest.js
@@ -3,7 +3,7 @@ const { expect } = require("chai");
 
 describe("MyDex", function(){
     let myDex;
-    const MyDexAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+    const MyDexAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
     before(async function() {
         const MyDex = await ethers.getContractFactory("MyDex");
@@ -15,7 +15,7 @@ describe("MyDex", function(){
     });
 
     describe("tx", function(){
-
+        
     });
 
 });
